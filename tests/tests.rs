c2e83a7cea77247e0fac821f677e4200b3103adf
GaveUp <gaveup@gmx.net>
2016-01-17 14:45:08 -0600

    Fix tests to build on stable.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,27 +1,21 @@
-#![feature(semaphore)]
 extern crate posix_ipc as ipc;
 
 use ipc::signals;
 use std::thread;
-use std::sync::{Arc, Semaphore, Mutex};
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
 #[test]
 fn raise_and_catch_with_closure() {
-    let mut caught: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
-    let mut sem: Arc<Semaphore> = Arc::new(Semaphore::new(1));
-    sem.acquire();
-    {
-        let sem = sem.clone();
-        let caught = caught.clone();
-        let f = move |s: signals::Signal| {
-            *caught.lock().unwrap() = true; sem.release()
-        };
-        unsafe {
-            signals::Signal::Usr1.handle(Box::new(f)).unwrap();
-        }
+    let caught: Arc<Mutex<bool>> = Arc::new(Mutex::new(false));
+    let caught2 = caught.clone();
+    let f = move |_| {
+        *caught2.lock().unwrap() = true;
+    };
+    unsafe {
+        signals::Signal::Usr1.handle(Box::new(f)).unwrap();
     }
     signals::Signal::Usr1.raise().unwrap();
-    sem.acquire();
     assert!(*caught.lock().unwrap());
 }
 
@@ -29,13 +23,13 @@ fn raise_and_catch_with_closure() {
 fn raise_and_catch_with_func() {
     static mut caught: bool = false;
     {
-        fn f(s: signals::Signal) {unsafe { caught = true }}
+        fn f(_: signals::Signal) {unsafe { caught = true }}
         unsafe {
             signals::Signal::Usr1.handle(Box::new(f)).unwrap();
         }
     }
     signals::Signal::Usr1.raise().unwrap();
-    thread::sleep_ms(500); // this is really racy :(
+    thread::sleep(Duration::from_millis(500)); // this is really racy :(
     assert!(unsafe { caught });
 }
 
